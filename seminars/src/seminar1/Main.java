cc3ab79f6ed20c6eb88fdbb0da3f9b995d39db1f
Alexander Styagov <sstyagov2@gmail.com>
2017-10-08 03:57:44 +0300

    Complement Main.java with tests for IQueues implementations

--- seminars/src/seminar1/Main.java
+++ seminars/src/seminar1/Main.java
@@ -1,31 +1,57 @@
 import collections.ArrayStack;
+import collections.IQueue;
+import collections.IStack;
+import collections.LinkedQueue;
 import collections.LinkedStack;
+import collections.TwoStackQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("=ArrayStack=");
-        ArrayStack<Character> astack = new ArrayStack<>();
+        System.out.println("ArrayStack");
+        IStack<Character> stack = new ArrayStack<>();
         for (int i = 0; i < 30; i++) {
-            astack.push((char) (i + 35));
+            stack.push((char) (i + 35));
         }
-        astack.print();
-        astack.pop();
-        astack.print();
-        astack.push('n');
-        astack.print();
-        System.out.println(astack.peek());
-        System.out.println("=LinkedStack=");
-        LinkedStack<Character> lstack = new LinkedStack<>();
+        stack.print();
+        for (int i = 0; i < 5; i++) {
+            stack.pop();
+        }
+        stack.push('n');
+        stack.print();
+        System.out.println("LinkedStack");
+
+        stack = new LinkedStack<>();
         for (int i = 0; i < 30; i++) {
-            lstack.push((char) (i + 35));
+            stack.push((char) (i + 35));
         }
-        lstack.print();
-        lstack.pop();
-        lstack.print();
-        lstack.push('n');
-        lstack.print();
-        System.out.println(lstack.peek());
+        stack.print();
+        for (int i = 0; i < 5; i++) {
+            stack.pop();
+        }
+        stack.push('n');
+        stack.print();
+
+        System.out.println("LinkedQueue");
+        IQueue<Character> queue = new LinkedQueue<>();
+        for (int i = 0; i < 30; i++) {
+            queue.enqueue((char) (i + 35));
+        }
+        queue.print();
+        for (int i = 0; i < 5; i++) {
+            queue.dequeue();
+        }
+        queue.enqueue('n');
+        queue.print();
+
+        System.out.println("TwoStackQueue");
+        queue = new TwoStackQueue<>();
+        for (int i = 0; i < 30; i++) {
+            queue.enqueue((char) (i + 35));
+        }
+        queue.print();
+        queue.enqueue('n');
+        queue.print();
 /*        System.out.println("LinkedStack");
         IStack<Integer> stack = new LinkedStack<>();
         for (int i = 0; i < 20; i++) {
