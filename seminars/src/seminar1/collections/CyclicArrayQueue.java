1bc102fba6ae5b82b6b133be55653e69a6ac7cf6
Alexander Styagov <sstyagov2@gmail.com>
2017-10-08 05:55:16 +0300

    Realized CyclicArrayQueue implementation of IQueue

--- seminars/src/seminar1/collections/CyclicArrayQueue.java
+++ seminars/src/seminar1/collections/CyclicArrayQueue.java
@@ -1,58 +1,98 @@
 package collections;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class CyclicArrayQueue<Item> implements collections.IQueue<Item> {
+public class CyclicArrayQueue<Item> implements IQueue<Item> {
+
+    private static final int DEFAULT_CAPACITY = 10;
 
     private Item[] elementData;
+    private int readPtr;
+    private int writePtr;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public CyclicArrayQueue() {
+        readPtr = 0;
+        writePtr = 0;
+        capacity = DEFAULT_CAPACITY;
+        elementData = (Item[]) new Object[capacity];
+    }
 
     @Override
     public void enqueue(Item item) {
-        /* TODO: implement it */
+        if (isFull())
+            grow();
+        elementData[writePtr] = item;
+        writePtr = (writePtr + 1) % capacity;
     }
 
     @Override
     public Item dequeue() {
-        /* TODO: implement it */
-        return null;
+        if (isEmpty())
+            throw new NoSuchElementException("Queue is empty");
+        if (capacity / size() > 4)
+            shrink();
+        Item tmp = elementData[readPtr];
+        elementData[readPtr] = null;
+        readPtr = (readPtr + 1) % capacity;
+        return tmp;
+    }
+
+    private boolean isFull() {
+        return readPtr == (writePtr + 1 ) % capacity;
     }
 
     @Override
     public boolean isEmpty() {
-        /* TODO: implement it */
-        return false;
+        return readPtr == writePtr;
     }
 
     @Override
     public int size() {
-        /* TODO: implement it */
-        return 0;
+        return (capacity - readPtr + writePtr) % capacity;
     }
 
     @Override
     public void print() {
-
+        System.out.print("Stack: ");
+        for (int i = readPtr; i < readPtr + size(); i++) {
+            System.out.print(elementData[i] + " ");
+        }
+        System.out.println();
     }
 
     private void grow() {
-        /**
-         * TODO: implement it
-         * Если массив заполнился,
-         * то увеличить его размер в полтора раз
-         */
+        changeCapacity(capacity *= 1.5);
     }
 
     private void shrink() {
-        /**
-         * TODO: implement it
-         * Если количество элементов в четыре раза меньше,
-         * то уменьшить его размер в два раза
-         */
+        changeCapacity(capacity /= 2);
+    }
+
+    private void changeCapacity(int newCapacity) {
+        elementData = Arrays.copyOf(elementData, newCapacity);
     }
 
     @Override
     public Iterator<Item> iterator() {
-        /* TODO: implement it */
-        return null;
+        return new ArrayStackIterator();
+    }
+
+    private class ArrayStackIterator implements Iterator<Item> {
+
+        private int currentPosition = size();
+
+        @Override
+        public boolean hasNext() {
+            return currentPosition != 0;
+        }
+
+        @Override
+        public Item next() {
+            return elementData[--currentPosition];
+        }
     }
 }
