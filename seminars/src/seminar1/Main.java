5c833bcdcad72557c794ced76aebf2205a7c7cf5
Alexander Styagov <sstyagov2@gmail.com>
2017-10-08 05:55:33 +0300

    Added tests in Main.java for CyclicArrayQueue

--- seminars/src/seminar1/Main.java
+++ seminars/src/seminar1/Main.java
@@ -1,4 +1,5 @@
 import collections.ArrayStack;
+import collections.CyclicArrayQueue;
 import collections.IQueue;
 import collections.IStack;
 import collections.LinkedQueue;
@@ -9,60 +10,71 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("ArrayStack");
-        IStack<Character> stack = new ArrayStack<>();
+        IStack<Integer> stack = new ArrayStack<>();
         for (int i = 0; i < 30; i++) {
-            stack.push((char) (i + 35));
+            stack.push(i);
         }
         stack.print();
         for (int i = 0; i < 5; i++) {
             stack.pop();
         }
-        stack.push('n');
+        stack.push(100);
         stack.print();
         System.out.println("LinkedStack");
 
         stack = new LinkedStack<>();
         for (int i = 0; i < 30; i++) {
-            stack.push((char) (i + 35));
+            stack.push(i);
         }
         stack.print();
         for (int i = 0; i < 5; i++) {
             stack.pop();
         }
-        stack.push('n');
+        stack.push(100);
         stack.print();
 
         System.out.println("LinkedQueue");
-        IQueue<Character> queue = new LinkedQueue<>();
+        IQueue<Integer> queue = new LinkedQueue<>();
         for (int i = 0; i < 30; i++) {
-            queue.enqueue((char) (i + 35));
+            queue.enqueue(i);
         }
         queue.print();
         for (int i = 0; i < 5; i++) {
             queue.dequeue();
         }
-        queue.enqueue('n');
+        queue.enqueue(100);
         queue.print();
 
         System.out.println("TwoStackQueue");
         queue = new TwoStackQueue<>();
         for (int i = 0; i < 30; i++) {
-            queue.enqueue((char) (i + 35));
+            queue.enqueue(i);
         }
         queue.print();
-        queue.enqueue('n');
+        for (int i = 0; i < 5; i++) {
+            queue.dequeue();
+        }
+        queue.enqueue(100);
+        queue.print();
+
+        System.out.println("CyclicArrayQueue");
+        queue = new CyclicArrayQueue<>();
+        for (int i = 0; i < 30; i++) {
+            queue.enqueue(i);
+        }
+        queue.print();
+        for (int i = 0; i < 5; i++) {
+            queue.dequeue();
+        }
+        queue.enqueue(100);
         queue.print();
 /*        System.out.println("LinkedStack");
         IStack<Integer> stack = new LinkedStack<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             stack.push(i);
         }
-//        for (int i : stack) {
-//            System.out.print(i + " ");
-//        }
-//        System.out.println();
-        for (int i = 0; i < 30; i++) {
-            System.out.print(stack.pop() + " ");
+        for (int i : stack) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
@@ -72,12 +84,8 @@ public class Main {
         for (int i = 0; i < 20; i++) {
             queue.enqueue(i);
         }
-//        for (int i : queue) {
-//            System.out.print(i + " ");
-//        }
-//        System.out.println();
-        for (int i = 0; i < 30; i++) {
-            System.out.print(queue.dequeue() + " ");
+        for (int i : queue) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
@@ -87,7 +95,7 @@ public class Main {
         for (int i = 0; i < 20; i++) {
             queue.enqueue(i);
         }
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.print(queue.dequeue() + " ");
         }*/
     }
