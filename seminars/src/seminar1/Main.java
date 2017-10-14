3641ef9da3972af90fa89f40e835aed13bd59a7f
Alexander Styagov <sstyagov2@gmail.com>
2017-10-14 08:28:29 +0300

    Stylized tests in Main

--- seminars/src/seminar1/Main.java
+++ seminars/src/seminar1/Main.java
@@ -12,133 +12,85 @@ import collections.TwoStackQueue;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ArrayStack");
-        IStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 30; i++) {
-            stack.push(i);
-        }
-        stack.print();
-        for (int i = 0; i < 5; i++) {
-            stack.pop();
-        }
-        stack.push(100);
-        stack.print();
-        System.out.println("LinkedStack");
+        myTests();
+    }
 
-        stack = new LinkedStack<>();
-        for (int i = 0; i < 30; i++) {
-            stack.push(i);
-        }
-        stack.print();
-        for (int i = 0; i < 5; i++) {
-            stack.pop();
-        }
-        stack.push(100);
-        stack.print();
+    private static void myTests() {
+        System.out.println("ArrayStack");
+        System.out.println("==========");
+        testStack(new ArrayStack<>());
+        System.out.println();
+
+        System.out.println("LinkedStack");
+        System.out.println("==========");
+        testStack(new LinkedStack<>());
+        System.out.println();
 
         System.out.println("LinkedQueue");
-        IQueue<Integer> queue = new LinkedQueue<>();
-        for (int i = 0; i < 30; i++) {
-            queue.enqueue(i);
-        }
-        queue.print();
-        for (int i = 0; i < 5; i++) {
-            queue.dequeue();
-        }
-        queue.enqueue(100);
-        queue.print();
+        System.out.println("==========");
+        testQueue(new LinkedQueue<>());
+        System.out.println();
 
         System.out.println("TwoStackQueue");
-        queue = new TwoStackQueue<>();
-        for (int i = 0; i < 30; i++) {
-            queue.enqueue(i);
-        }
-        queue.print();
-        for (int i = 0; i < 5; i++) {
-            queue.dequeue();
-        }
-        queue.enqueue(100);
-        queue.print();
+        System.out.println("==========");
+        testQueue(new TwoStackQueue<>());
+        System.out.println();
 
         System.out.println("CyclicArrayQueue");
-        queue = new CyclicArrayQueue<>();
-        for (int i = 0; i < 30; i++) {
-            queue.enqueue(i);
-        }
-        queue.print();
-        for (int i = 0; i < 5; i++) {
-            queue.dequeue();
-        }
-        queue.enqueue(100);
-        queue.print();
+        System.out.println("==========");
+        testQueue(new CyclicArrayQueue<>());
+        System.out.println();
 
         System.out.println("LinkedDeque");
-        IDeque<Integer> deque = new LinkedDeque<>();
-        for (int i = 0; i < 15; i++) {
-            deque.pushBack(i);
-        }
-        for (int i = 30; i > 10; i--) {
-            deque.pushFront(i);
-        }
-        deque.print();
-        for (int i = 0; i < 5; i++) {
-            deque.popBack();
-            deque.popFront();
-        }
-        deque.print();
+        System.out.println("==========");
+        testDeque(new LinkedDeque<>());
+        System.out.println();
 
         System.out.println("CyclicArrayDeque");
-        deque = new CyclicArrayDeque<>();
-        for (int i = 0; i < 10; i++) {
+        System.out.println("==========");
+        testDeque(new CyclicArrayDeque<>());
+        System.out.println();
+    }
+
+    private static void testStack(IStack<Integer> stack) {
+        for (int i = 0; i < 100; i++)
+            stack.push(i);
+
+        stack.print();
+
+        for (int i = 0; i < 20; i++)
+            stack.pop();
+
+        stack.push(100);
+        stack.print();
+    }
+
+    private static void testQueue(IQueue<Integer> queue) {
+        for (int i = 0; i < 30; i++)
+            queue.enqueue(i);
+
+        queue.print();
+
+        for (int i = 0; i < 5; i++)
+            queue.dequeue();
+
+        queue.enqueue(100);
+        queue.print();
+    }
+
+    private static void testDeque(IDeque<Integer> deque) {
+        for (int i = 0; i < 100; i++) {
             deque.pushFront(i);
             deque.pushBack(++i);
         }
-        ((CyclicArrayDeque) deque).printArray();
-        for (int i = 0; i < 5; i++) {
-            deque.popBack();
-            deque.popFront();
-            ((CyclicArrayDeque) deque).printArray();
-        }
-        /*for (int i = 30; i > 10; i--) {
-            deque.pushBack(i);
-        }
-        deque.print();
-        for (int i = 0; i < 5; i++) {
-            deque.popBack();
-            deque.popFront();
-        }
-        deque.print();*/
 
-       /* //System.out.println("Iterator 1");
-        IncreasingIterator iterator1 = new IncreasingIterator(0, 5, 10);
-       *//* IncreasingIterator tmp1 = iterator1;
-        System.out.println("b4 1: " + tmp1.hasNext());
+        deque.print();
+
         for (int i = 0; i < 20; i++) {
-            System.out.print(iterator1.next());
-            if (i < 19) {
-                System.out.print(" -> ");
-            }
-        }*//*
-        //System.out.println();
-        //System.out.println("Iterator 2");
-        IncreasingIterator iterator2 = new IncreasingIterator(2, 4, 10);
-        *//*IncreasingIterator tmp2 = iterator2;
-        System.out.println("b4 2: " + tmp2.hasNext());
-        for (int i = 0; i < 20; i++) {
-            System.out.print(iterator2.next());
-            if (i < 19) {
-                System.out.print(" -> ");
-            }
-        }*//*
-        System.out.println();
-        System.out.println("Merging iterator");
-        MergingIncreasingIterator increasingIterator = new MergingIncreasingIterator(iterator1, iterator2);
-        //System.out.println("1: " + tmp1.hasNext() + "; 2: " + tmp2.hasNext());
-        for (int i = 0; i < 30; i++) {
-            System.out.print(increasingIterator.next());
-            if (i < 19) {
-                System.out.print(" -> ");
-            }
-        }*/
+            deque.popBack();
+            deque.popFront();
+        }
+
+        deque.print();
     }
 }
