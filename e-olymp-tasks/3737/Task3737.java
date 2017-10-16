493de37b75e4e8d96480de3c96b9530dac36d677
Alexander Styagov <sstyagov2@gmail.com>
2017-10-16 04:34:45 +0300

    Solved task #3737 from e-olymp (Heap?)

--- e-olymp-tasks/3737/Task3737.java
+++ e-olymp-tasks/3737/Task3737.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Куча ли?
  * Структуру данных Heap можно реализовать на основе массива.
@@ -7,12 +9,34 @@
  * Дан массив целых чисел. Определите является ли он Heapом.
  *
  * Входные данные
- * Первая строка содержит целое число n (1 ≤ n ≤ 105). Вторая строка содержит n целых чисел, не превосходящих по модулю 2·109.
+ * Первая строка содержит целое число n (1 ≤ n ≤ 10^5). Вторая строка содержит n целых чисел, не превосходящих по модулю 2·10^9.
  *
  * Выходные данные
  * Выведите "YES", если массив является Heapом и "NO" в противном случае.
  */
 public class Task3737 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        long[] numbers = new long[count];
+
+        for (int i = 0; i < count; i++)
+            numbers[i] = in.nextLong();
+
+        System.out.println(isHeap(numbers)? "YES" : "NO");
+    }
+
+    private static boolean isHeap(long arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            if  (
+                    (2 * i + 1 < arr.length && arr[i] > arr[2 * i + 1])
+                    ||
+                    (2 * i + 2 < arr.length && arr[i] > arr[2 * i + 2])
+                )
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
